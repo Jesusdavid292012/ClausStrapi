121085ba92b90542f4d1a9e10f89dec68ec923e2
Jesus Peña <jdpfragoso@gmail.com>
2022-01-18 15:11:32 -0500

    tree commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3f65ff870f3a37061c1c55c16ac74300'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2c5da92c-9fa5-48eb-852e-8e500a309f1c'
+};
